936b6728185accfddf9ce0be9562f5e9a0fb0ff3
HanSu Lee <i.hansu.lee@gmail.com>
2023-02-19 08:00:43 +0900

    feat(demo): add radioe, buttons for toast control/test

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Toast } from '../../';
+import { useToast, ToastPosition, ToastType } from '../../';
 
 export const App: React.FC = () => {
+  const toast = useToast();
+  const [type, setType] = useState<ToastType | 'none'>('none');
+
   return (
     <div style={{ width: '100%', height: '100%', padding: '3rem' }}>
-      <Toast
-        emoji="🚧"
-        emojiBackground="#F3E45C"
-        text={['@hanseo0507/react-toast', 'Awesome Notification']}
-      />
-      <Toast
-        emoji="🚨"
-        emojiBackground="#F3655C"
-        text={['@hanseo0507/react-toast', 'Awesome Notification']}
-      />
-      <Toast
-        emoji="🎉"
-        emojiBackground={['70deg', '#ECA0FF', '#778DFF']}
-        text={['@hanseo0507/react-toast', 'Awesome Notification']}
-      />
+      <div style={{ display: 'flex', color: 'white', gap: '12px' }}>
+        <input
+          id="none"
+          type="radio"
+          name="none"
+          value="none"
+          checked={type === 'none'}
+          onChange={(e) => setType(e.target.value as 'none')}
+        />
+        none
+        {Object.values(ToastType).map((_type) => (
+          <>
+            <input
+              id={_type}
+              type="radio"
+              value={_type}
+              name={_type}
+              checked={_type === type}
+              onChange={(e) => setType(e.target.value as ToastType)}
+            />
+            {_type}
+          </>
+        ))}
+      </div>
+      <div>
+        {Object.values(ToastPosition).map((position) => (
+          <button
+            key={position}
+            onClick={() => {
+              const text = ['@hanseo0507/react-toast', 'Awesome Notification'] as [string, string];
+
+              if (type === 'none')
+                return toast(
+                  { emoji: '🎉', emojiBackground: ['70deg', '#ECA0FF', '#778DFF'], text },
+                  { position }
+                );
+              return toast[type]({ text }, { position });
+            }}
+          >
+            {position}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { ToastProvider } from '../../';
+
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ToastProvider>
     <App />
-  </React.StrictMode>
+  </ToastProvider>
 );
