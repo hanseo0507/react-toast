a75218defc1d093827dc5cb6d62c74050a7de920
HanSu Lee <i.hansu.lee@gmail.com>
2023-02-19 03:53:23 +0900

    chore(deps): update demo dependencies to link:*

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
+import { Toast } from '../../';
+
 export const App: React.FC = () => {
-  return <div />;
+  return (
+    <div style={{ width: '100%', height: '100%', padding: '3rem' }}>
+      <Toast
+        emoji="🚧"
+        emojiBackground="#F3E45C"
+        text={['@hanseo0507/react-toast', 'Awesome Notification']}
+      />
+      <Toast
+        emoji="🚨"
+        emojiBackground="#F3655C"
+        text={['@hanseo0507/react-toast', 'Awesome Notification']}
+      />
+      <Toast
+        emoji="🎉"
+        emojiBackground={['70deg', '#ECA0FF', '#778DFF']}
+        text={['@hanseo0507/react-toast', 'Awesome Notification']}
+      />
+    </div>
+  );
 };
